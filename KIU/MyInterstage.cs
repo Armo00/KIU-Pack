90e7ae4963785836b45b278b71ea6c90218500e3
Old-Copper <2587932768@qq.com>
2026-07-03 22:06:47 +0800

    interstage change

---
+++ KIU/MyInterstage.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace YourMod
+{
+    public class MyInterstage : PartModule
+    {
+        private ModuleAnimateGeneric anim;
+
+        public override void OnStart(StartState state)
+        {
+            base.OnStart(state);
+            anim = part.FindModuleImplementing<ModuleAnimateGeneric>();
+        }
+
+        public override void OnActive()
+        {
+            base.OnActive();
+            if (anim != null)
+            {
+                anim.Toggle();
+            }
+        }
+    }
+}
